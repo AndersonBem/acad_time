d6b5d316b7e3a3d60c44c75ae7a2a9f206d9f21a
AndersonBem <126602982+AndersonBem@users.noreply.github.com>
2026-04-14 10:45:49 -0300

    Add Netlify app to CORS allowed origins

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -78,6 +78,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:5500",
     "http://localhost:5500",
+    "http://acadtime.netlify.app"
 ]
 
 CORS_ALLOW_CREDENTIALS = True
