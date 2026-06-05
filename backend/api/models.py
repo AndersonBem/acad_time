f349044fc60ca6e0a6f97211a4f5400ecc05f36f
AndersonBem <126602982+AndersonBem@users.noreply.github.com>
2026-06-05 13:42:24 -0300

    Pequenas correções

--- backend/api/models.py
+++ backend/api/models.py
@@ -198,6 +198,7 @@ class TurnoCurso(models.Model):
 class TipoAtividade(models.Model):
     id_tipo_atividade = models.AutoField(db_column='idTipoAtividade', primary_key=True)
     nome = models.CharField(max_length=30, unique=True)
+    descricao = models.TextField(blank=True, null=True)
 
     class Meta:
         managed = False
